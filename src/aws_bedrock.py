a243f5bb69386de580f731ed09abb057625766c6
pavel sumetc <pavel.sumetc@fuel50.com>
2025-09-04 11:48:04 +1200

    clean up

--- src/aws_bedrock.py
+++ src/aws_bedrock.py
@@ -5,9 +5,11 @@ A module to handle the Amazon Bedrock models
 
 import boto3
 from langchain_aws import ChatBedrockConverse
+import os
 
 def get_llm_model() -> ChatBedrockConverse:
-    session = boto3.Session(profile_name="smoke", region_name="us-west-2")
+    profile = os.getenv("AWS_PROFILE","")
+    session = boto3.Session(profile_name=profile, region_name="us-west-2")
     client = session.client("bedrock-runtime")
     model_name = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
     model = ChatBedrockConverse(
